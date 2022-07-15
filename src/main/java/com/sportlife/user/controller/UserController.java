8fca4e48a51d37465f46f5876a48b7326012d785
jcaceres@todo1.com <jcaceres@todo1.com>
2022-07-14 23:32:52 -0500

    Aplicado madurez de  Richarson y Response Entity al usuario

--- src/main/java/com/sportlife/user/controller/UserController.java
+++ src/main/java/com/sportlife/user/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.sportlife.user.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.sportlife.user.model.User;
 import com.sportlife.user.service.IUserService;
@@ -23,28 +27,33 @@ public class UserController {
 	private IUserService iUserService;
 
 	@GetMapping("/{id}")
-	public User findById(@PathVariable("id") Integer id) {
-		return iUserService.findById(id);
+	public ResponseEntity<User> findById(@PathVariable("id") Integer id) {
+		return new ResponseEntity<>(iUserService.findById(id), HttpStatus.OK);
 	}
 
 	@GetMapping
-	public List<User> findAll() {
-		return iUserService.findAll();
+	public ResponseEntity<List<User>> findAll() {
+		List<User> users= iUserService.findAll();
+		return new ResponseEntity<>(users, HttpStatus.OK);
 	}
 
 	@PostMapping
-	public User save(@RequestBody User user) {
-		return iUserService.save(user);
+	public ResponseEntity<Void> save(@RequestBody User user) {
+		User userSave= iUserService.save(user);
+		URI uriUserLocation = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userSave.getIdUser()).toUri();
+		return ResponseEntity.created(uriUserLocation).build();
+		//return new ResponseEntity<>(iUserService.save(user), HttpStatus.CREATED);
 	}
 
 	@PutMapping
-	public User update(@RequestBody User user) {
-		return iUserService.update(user);
+	public ResponseEntity<User> update(@RequestBody User user) {
+		return new ResponseEntity<>(iUserService.update(user), HttpStatus.OK);
 	}
 
 	@DeleteMapping("/{id}")
-	public void deleteById(@PathVariable("id") Integer id) {
+	public ResponseEntity<Void> deleteById(@PathVariable("id") Integer id) {
 		iUserService.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
 }
